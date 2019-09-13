b43f8ec250e0651ee746c9eb041466bf21c31642
hhj227 <gamcho227@naver.com>
2019-09-13 17:01:01 +0900

    crypto

--- crypto.js
+++ crypto.js
@@ -1,5 +1,14 @@
 const crypto = require('crypto');
 
+const cipher = crypto.createCipher('aes-256-cbc','key');
+let result = cipher.update('fool','utf8','base64');
+result += cipher.final('base64');
+console.log('암호',result);
+const decipher = crypto.createDecipher('aes-256-cbc','key');
+let result2 = decipher.update(result, 'base64', 'utf8');
+result2 += decipher.final('utf8');
+console.log('평문',result2);
+
 crypto.randomBytes(64, (err, buf) => {
     const salt = buf.toString('base64');
     console.log("salt = ", salt);
