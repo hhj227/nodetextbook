8b8bf26af58a4c8636887fa654b3f5cd03c94ff8
hhj227 <gamcho227@naver.com>
2019-10-05 23:51:31 +0900

    add html and css

--- http module web server/server1.js
+++ http module web server/server1.js
@@ -12,6 +12,10 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
+const session = {
+
+};
+
 const server = http.createServer((req, res) => {
     const cookies = parseCookies(req.headers.cookie);
     console.log(req.url, cookies);
@@ -19,15 +23,20 @@ const server = http.createServer((req, res) => {
         const { query } = url.parse(req.url);
         const { name } = qs.parse(query);
         const expires = new Date();
-        expires.setMinutes(expires.getMinutes()+5);
+        expires.setMinutes(expires.getMinutes() + 5);
+        const randomInt = Date.now();
+        session[randomInt] = {
+            name,
+            expires,
+        };
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
-    } else if (cookies.name) {
+    } else if (cookies.session && session[cookies.session].expires > new Date()) {
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요`);
+        res.end(`${session[cookies.session].name}님 안녕하세요`);
     }
     else {
         fs.readFile('./server.html', (err, data) => {
